04d971d0a2a68fd2bac237848f676cb40c9ea66f
Vagus-art <agustin.favo@gmail.com>
2020-02-29 15:13:19 -0300

    created react app and added firebase

---
+++ todo/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBOgjnsqu4nNhHlBb0nuhiYhSEPdFtczAA",
+    authDomain: "todolist-97865.firebaseapp.com",
+    databaseURL: "https://todolist-97865.firebaseio.com",
+    projectId: "todolist-97865",
+    storageBucket: "todolist-97865.appspot.com",
+    messagingSenderId: "34774874521",
+    appId: "1:34774874521:web:c4b575dcf0d06dcb88fb08"
+  };
+
+module.exports = firebaseConfig;
