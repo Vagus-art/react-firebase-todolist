ee4c7beff0829f3437dc91220495b788d2b62a55
Vagus-art <agustin.favo@gmail.com>
2020-03-02 08:11:47 -0300

    absolute imports work

--- todo/firebase.js
+++
@@ -1,7 +0,0 @@
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-const firebase = require("firebase/app");
-const firebaseConfig = require("./firebaseConfig");
-firebase.initializeApp(firebaseConfig);
-
-module.exports=firebase;
---
+++ todo/src/services/firebase/firebase.js
@@ -0,0 +1,9 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from 'firebase';
+import firebaseConfig from './firebaseConfig'
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
